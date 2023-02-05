21d417347f58dbbef58b4672384b7b1ff6b6d73c
Julian <Julian@Nox>
2023-02-05 12:25:14 +0100

    .

--- src/main/python/ba_trees/gui/selection_widget/SelectionPointsInImageWidget.py
+++ src/main/python/ba_trees/gui/selection_widget/SelectionPointsInImageWidget.py
@@ -7,7 +7,6 @@ from ba_trees.gui.selection_widget import (SelectionInformation, SelectedImageWi
 from ba_trees.gui.selection_widget.SelectionInformation import Point, Image
 from ba_trees.gui.selection_widget.SelectionPointsInImageWidgetSetup import Ui_Form
 from ba_trees.gui.background.qt.QtFunctions import QtFunctions
-from scipy.constants._constants import point
 
 
 class SelectionPointsInImageWidget(QWidget):
